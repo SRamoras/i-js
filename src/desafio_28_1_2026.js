38d370ec62e5820f2535703c6c0e7f2071820c53
S1lva <diogomcsilva5@gmail.com>
2026-01-28 22:29:51 +0000

    feat: add desafio_28_1_2026

---
+++ src/desafio_28_1_2026.js
@@ -0,0 +1,35 @@
+// message = ""
+// num = 10
+
+// message = num >= 10 && num <= 20 && num !== 15 
+//     ? "Esta entre 10 e 20 e nao é 15" 
+//     : ""
+
+// console.log(message)
+
+
+
+
+// desafio adicional
+// function haddleNum(num){
+//     message = "Nao faz nada"
+
+//     message = num >= 10 && num <= 20 && num !== 15 
+//         ? "Esta entre 10 e 20 e nao é 15" 
+//         : ""
+    
+//     message = num % 5 === 0 
+//         ? "O numero é multiplo de 5" 
+//         : message
+
+//     return message
+// }
+// var prompt = require('prompt-sync')();
+
+// var num = prompt('Insira um numero? ');
+// console.log(haddleNum(num))
+
+let logado = true
+if(!logado){
+     console.log("Não está logado");
+}
